c54fdbaa7a34c370674af23afbca5e8474cb8c04
riz <me@rizalishan.com>
2018-02-03 14:37:53 +0100

    Bit of refactoring

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,22 +1,22 @@
 const webpack = require('webpack')
 const clientConfig = require('./webpack.client.config')
 
-module.exports = function setupDeServer(app){
-    clientConfig.entry.app = [
-        'webpack-hot-middleware/client',
-        clientConfig.entry.app
-    ]
-    clientConfig.plugins.push(
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin()
-    )
-    const clientCompiler = webpack(clientConfig)
-    app.use(
-        require('webpack-dev-middleware')(clientCompiler,{
-            state: {
-                colors: true
-            }
-        })
-    )
-    app.use(require('webpack-hot-middleware')(clientCompiler))
+module.exports = function setupDeServer (app) {
+  clientConfig.entry.app = [
+    'webpack-hot-middleware/client',
+    clientConfig.entry.app
+  ]
+  clientConfig.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  )
+  const clientCompiler = webpack(clientConfig)
+  app.use(
+    require('webpack-dev-middleware')(clientCompiler, {
+      state: {
+        colors: true
+      }
+    })
+  )
+  app.use(require('webpack-hot-middleware')(clientCompiler))
 }
