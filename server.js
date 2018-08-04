98cc7957d2f24dab68fc3d727ede9431af034ff6
riz <me@rizalishan.com>
2018-08-04 11:56:23 +0200

    Adding more requests per 15 minutes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const RateLimit = require('express-rate-limit')
 
 var limiter = new RateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10, // limit each IP to 100 requests per windowMs
+  max: 100, // limit each IP to 100 requests per windowMs
   delayMs: 0 // disable delaying - full speed until the max limit is reached
 })
 
